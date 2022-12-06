4b2bdf83fed1a9f13ea51877b42df8ae6fed6916
git_manasse8002 <manassemburu@gmail.com>
2022-12-06 15:54:03 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Durant", "Harden", "Curry"];
+const writeCards = function(names, event){
+    let giftMsgs = [];
+    for (let i = 0; i < names.length; i++){
+        let giftMsg = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        giftMsgs.push(giftMsg)
+    }
+
+    return giftMsgs;
+}
+writeCards(names, "greetings")
+
+const countDown = function(n){
+    let i = n;
+    while(i >= 0){
+        console.log(i);
+        i--
+    }
+}
+
+countDown(10);
